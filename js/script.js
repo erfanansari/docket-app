33eed0f0ce85e4b414049780d2b5f41bb518459c
erfan <erfanansarimehr.47@gmail.com>
2021-02-04 18:06:49 +0330

    implement time

--- js/script.js
+++ js/script.js
@@ -2,50 +2,71 @@
 // assigning components to variables
 const modalInput = document.querySelector('.modal-input');
 const mainNoteDivChild = document.querySelector('.main-notes').childNodes[1];
-const welcomeDiv = document.querySelector('.welcome-div');
-const continueBtn = document.querySelector('#continue');
 const addNoteBtn = document.querySelector('#add-note');
 
 /*==========  insert Notes in DOM  ==========*/
 addNoteBtn.addEventListener('click', function () {
-  const html = `<div
-  class="card col-md-6 mx-2 my-2 note note ${mainNoteDivChild.children.length}"
-  style="transition: all ease 0.3s; max-width: 23%"
->
-  <div class="card-body">
-    <div class="d-flex justify-content-center">
-      <button class="btn m-0 p-0 shadow-none delete-note">
-        <i class="fa fa-times"></i>
-      </button>
-      <input
-        type="text"
-        class="card-title mt-0 text-center note-title"
-        maxlength="17"
-        value="Note title"
-      />
-      <div class="visually-hidden">
-        <span class="change-color"><i class="fa fa-chevron-left"></i></span>
-        <ul class="position-absolute list-unstyled change-color-ul">
-          <li>1</li>
-          <li>2</li>
-          <li>3</li>
-          <li>4</li>
-        </ul>
-      </div>
-    </div>
-    <label for=""></label>
-    <textarea class="" name="" id="" cols="30" rows="8"></textarea>
-    <div class="d-flex justify-content-between align-items-center mb-4 mt-2">
-      <p class="m-0 date-p">May 25,2020</p>
-      <button
-        class="btn shadow-none m-0 p-0 btn-note d-flex justify-content-center align-items-center"
-      >
-        <i class="fas fa-pen d-none edit-btn"></i
-        ><i class="fa fa-check check-btn"></i>
-      </button>
-    </div>
-  </div>
-</div>`;
+  /*==========  implement time  ==========*/
+  const now = new Date();
+  const options = {
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric',
+  };
+  const hour = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const format = new Intl.DateTimeFormat('en-US', options).format(now);
+
+  const html = `<div class="card col-md-6 mx-2 my-2 note transition">
+                    <div class="card-body">
+                      <div class="d-flex justify-content-center">
+                        <button class="btn m-0 p-0 shadow-none delete-note">
+                          <i class="fa fa-times"></i>
+                        </button>
+                        <input
+                          type="text"
+                          class="card-title mt-0 text-center note-title"
+                          maxlength="17"
+                          value="Note title"
+                        />
+                        <div class="visually-hidden">
+                          <span class="change-color"
+                            ><i class="fa fa-chevron-left"></i
+                          ></span>
+                          <ul
+                            class="position-absolute list-unstyled change-color-ul"
+                          >
+                            <li>1</li>
+                            <li>2</li>
+                            <li>3</li>
+                            <li>4</li>
+                          </ul>
+                        </div>
+                      </div>
+                      <label for=""></label>
+                      <textarea
+                        class=""
+                        name=""
+                        id=""
+                        cols="30"
+                        rows="8"
+                      ></textarea>
+                      <div
+                        class="d-flex justify-content-between align-items-center mb-4 mt-2"
+                      >
+                        <div class="d-flex flex-column justify-content-center">
+                          <p class="m-0 date-p">${format}</p>
+                          <small class="">${hour}:${minutes}</small>
+                        </div>
+                        <button
+                          class="btn shadow-none m-0 p-0 btn-note d-flex justify-content-center align-items-center"
+                        >
+                          <i class="fas fa-pen d-none edit-btn"></i
+                          ><i class="fa fa-check check-btn"></i>
+                        </button>
+                      </div>
+                    </div>
+                  </div>`;
   mainNoteDivChild.insertAdjacentHTML('afterbegin', html);
 
   const textArea = document.querySelector('textarea');
